591881a2c64aedf8bfa101895bd139fa30f62a0c
Andrew Clark <cap@mail.com>
2024-05-02 18:24:47 +0200

    Add identity provider.

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -28,13 +28,72 @@ resource "aws_cognito_user_pool_client" "cognito_user_pool_client" {
 
 resource "aws_cognito_identity_pool" "cognito_identity_pool" {
   identity_pool_name = "${var.app}-identity-pool-${var.stage}"
-
+  # No anoymous users
+  allow_unauthenticated_identities = false
   # Specify supported Cognito User Pool(s)
   cognito_identity_providers {
     client_id               = aws_cognito_user_pool_client.cognito_user_pool_client.id
     provider_name           = "cognito-idp.${var.region}.amazonaws.com/${aws_cognito_user_pool.cognito_user_pool.id}"
     server_side_token_check = false
   }
+}
 
 
+
+resource "aws_cognito_identity_pool_roles_attachment" "cognito_identity_pool_roles_attachment" {
+  identity_pool_id = aws_cognito_identity_pool.cognito_identity_pool.id
+
+  roles = {
+    "authenticated"   = aws_iam_role.authenticated_role.arn
+    "unauthenticated" = aws_iam_role.unauthenticated_role.arn
+  }
+}
+
+resource "aws_iam_role" "authenticated_role" {
+  name = "${var.app}-authenticated-role-${var.stage}"
+  assume_role_policy = jsonencode(
+    {
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Action = "sts:AssumeRoleWithWebIdentity",
+          Principal = {
+            Federated = "cognito-identity.amazonaws.com"
+          },
+          Effect = "Allow",
+          Condition = {
+            StringEquals = {
+              "cognito-identity.amazonaws.com:aud" : aws_cognito_identity_pool.cognito_identity_pool.id
+            }
+          }
+        }
+      ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "authenticated_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess" # Full access to DB's TODO we need to limit this (the best way is the api gateway)
+  role       = aws_iam_role.authenticated_role.name
+}
+
+resource "aws_iam_role" "unauthenticated_role" {
+  name = "${var.app}-unauthenticated-role-${var.stage}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Effect = "Allow",
+        Principal = {
+          Federated = "cognito-identity.amazonaws.com"
+        },
+        Condition = {
+          StringEquals = {
+            "cognito-identity.amazonaws.com:aud" : aws_cognito_identity_pool.cognito_identity_pool.id
+          }
+        }
+      }
+    ]
+  })
 }
