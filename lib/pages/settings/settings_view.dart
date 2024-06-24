05e0c8878c667b90125fba0b40bef72ab376ee5a
Andrew Clark <cap@mail.com>
2024-06-24 21:41:42 +0200

    Adds some padding to settings screen

--- lib/pages/settings/settings_view.dart
+++ lib/pages/settings/settings_view.dart
@@ -12,46 +12,51 @@ class SettingsView extends StatefulWidget {
 class _SettingsViewState extends State<SettingsView> {
   @override
   Widget build(BuildContext context) {
-    return SettingsScreen(
-      title: 'Application Settings',
-      children: [
-        SettingsGroup(title: 'Adminstration', children: <Widget>[
-          SimpleSettingsTile(
-            title: 'Synchronize with Consigncloud',
-            subtitle: 'General App Settings',
+    return Scaffold(
+        body: Padding(
+            padding: const EdgeInsets.all(25),
             child: SettingsScreen(
-              title: 'Synchronize with Consigncloud',
-              children: <Widget>[
-                const TextInputSettingsTile(
-                  title: 'api key',
-                  settingKey: 'cc-sync-api-key',
-                  obscureText: true,
-                  borderColor: Colors.blueAccent,
-                  errorColor: Colors.deepOrangeAccent,
-                ),
-                TextInputSettingsTile(
-                  title: 'limit',
-                  settingKey: 'cc-sync-limit',
-                  borderColor: Colors.blueAccent,
-                  errorColor: Colors.deepOrangeAccent,
-                  initialValue: '0',
-                  validator: (String? limit) {
-                    if (int.tryParse(limit ?? '') != null) {
-                      return null;
-                    }
-                    return "Must be an integer";
-                  },
-                ),
-                SimpleSettingsTile(
-                  title: 'Custom Settings',
-                  subtitle: 'Tap to execute custom callback',
-                  onTap: () => safePrint('Custom action'),
-                ),
+              title: 'Application Settings',
+              children: [
+                SettingsGroup(title: 'Adminstration', children: <Widget>[
+                  SimpleSettingsTile(
+                      title: 'Synchronize with Consigncloud',
+                      subtitle: 'General App Settings',
+                      child: Padding(
+                        padding: const EdgeInsets.all(25),
+                        child: SettingsScreen(
+                          title: 'Synchronize with Consigncloud',
+                          children: <Widget>[
+                            const TextInputSettingsTile(
+                              title: 'api key',
+                              settingKey: 'cc-sync-api-key',
+                              obscureText: true,
+                              borderColor: Colors.blueAccent,
+                              errorColor: Colors.deepOrangeAccent,
+                            ),
+                            TextInputSettingsTile(
+                              title: 'limit',
+                              settingKey: 'cc-sync-limit',
+                              borderColor: Colors.blueAccent,
+                              errorColor: Colors.deepOrangeAccent,
+                              initialValue: '0',
+                              validator: (String? limit) {
+                                if (int.tryParse(limit ?? '') != null) {
+                                  return null;
+                                }
+                                return "Must be an integer";
+                              },
+                            ),
+                            SimpleSettingsTile(
+                              title: 'Custom Settings',
+                              subtitle: 'Tap to execute custom callback',
+                              onTap: () => safePrint('Custom action'),
+                            ),
+                          ],
+                        ),
+                      )),
+                ]),
               ],
-            ),
-          ),
-        ]),
-      ],
-    );
+            )));
   }
 }
