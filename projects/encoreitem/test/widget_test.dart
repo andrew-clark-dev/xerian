17b3b676aecfaf2c72f39e1f1d5f455275177a28
Andrew Clark <cap@mail.com>
2024-07-17 22:53:58 +0200

    Added login to Item

--- projects/encoreitem/test/widget_test.dart
+++ projects/encoreitem/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:encoreitem/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(EncoreItem());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
